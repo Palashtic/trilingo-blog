28ebbcc79a72122c082c1e3aa15a1d6b3be033cc
parthchat99 <parthchat99@github.com>
2019-12-09 12:13:15 +0530

    Project setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BlogsComponent } from './blogs/blogs/blogs.component';
+import { BlogDetailsComponent } from './blogs/blog-details/blog-details.component';
+import { IndexPageComponent } from './blogs/index-page/index-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogsComponent,
+    BlogDetailsComponent,
+    IndexPageComponent
   ],
   imports: [
     BrowserModule
